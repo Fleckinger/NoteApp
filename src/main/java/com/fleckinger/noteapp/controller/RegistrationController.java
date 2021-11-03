f69a7ddd2c6b8608b4b28e0e9d71c80d8ec42dd0
Vladislav Safarov <egbertz@yandex.ru>
2021-11-03 15:51:05 +0300

    registration controller

--- src/main/java/com/fleckinger/noteapp/controller/RegistrationController.java
+++ src/main/java/com/fleckinger/noteapp/controller/RegistrationController.java
@@ -1,14 +1,24 @@
 package com.fleckinger.noteapp.controller;
 
 import com.fleckinger.noteapp.entity.user.User;
+import com.fleckinger.noteapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RegistrationController {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public RegistrationController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/registration")
     public String registrationForm(Model model) {
@@ -17,8 +27,15 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String registrationSubmit(@ModelAttribute User user, Model model) {
+    public ModelAndView registrationSubmit(@ModelAttribute User user, Model model) {
         model.getAttribute("user");
+        if (!user.getPassword().equals(user.getPasswordConfirm())) {
+            model.addAttribute("passwordError", "Пароли не совпадают");
+            //TODO не выводит надпись о несовпадении паролей
+            return new ModelAndView("registration");
+        }
 
+        userRepository.save(user);
+        return new ModelAndView("redirect:note/all");
     }
 }
