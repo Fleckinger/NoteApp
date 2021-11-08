091ae80695d46b71ccc551af5ca03a75d8075490
Vladislav Safarov <egbertz@yandex.ru>
2021-11-08 15:10:28 +0300

    implement CRUD methods

--- src/main/java/com/fleckinger/noteapp/service/note/NoteService.java
+++ src/main/java/com/fleckinger/noteapp/service/note/NoteService.java
@@ -1,25 +1,93 @@
 package com.fleckinger.noteapp.service.note;
 
 import com.fleckinger.noteapp.entity.note.Note;
+
+import com.fleckinger.noteapp.entity.note.NoteStatus;
 import com.fleckinger.noteapp.entity.user.User;
 import com.fleckinger.noteapp.repository.NoteRepository;
+import com.fleckinger.noteapp.security.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
-@Configuration
-public class NoteServiceImpl {
-
+@Service
+public class NoteService {
 
     private final NoteRepository noteRepository;
 
-    @Autowired
+    private final UserDetailServiceImpl userDetailService;
 
-    public NoteServiceImpl(NoteRepository noteRepository) {
+    @Autowired
+    public NoteService(NoteRepository noteRepository, UserDetailServiceImpl userDetailsService) {
         this.noteRepository = noteRepository;
+        this.userDetailService = userDetailsService;
     }
-    //TODO реализовать логику заметок, от получения из БД до каких-то служебных методов. см. UserService
+
+    public void save(Note note) {
+        noteRepository.save(note);
+    }
+
+    public Note get(long id) {
+        return noteRepository.findById(id)
+                .orElseThrow(() -> new UsernameNotFoundException("Note not found"));
+    }
+
+    public List<Note> getAll(long userId) {
+
+        return noteRepository.findAllByUserId(userId);
+    }
+
+    public void update(Note note) {
+        noteRepository.save(note);
+    }
+
+    public void update(long id, NoteStatus status, String content) {
+        Note note = get(id);
+        note.setStatus(status);
+        note.setContent(content);
+        note.setUploadDate(LocalDateTime.now());
+        noteRepository.save(note);
+    }
+
+    public void updateStatus(long id, NoteStatus status) {
+        Note note = get(id);
+        note.setStatus(status);
+        note.setUploadDate(LocalDateTime.now());
+        noteRepository.save(note);
+    }
+
+    public void updateContent(long id, String content) {
+        Note note = get(id);
+        note.setContent(content);
+        note.setUploadDate(LocalDateTime.now());
+        noteRepository.save(note);
+    }
+
+    public void delete(long id) {
+        noteRepository.deleteById(id);
+    }
+
+    public void setDeleteStatus(long id) {
+        Note note = get(id);
+        note.setStatus(NoteStatus.DELETED);
+        save(note);
+    }
+
+    public void archive(long id) {
+        Note note = get(id);
+        note.setStatus(NoteStatus.ARCHIVED);
+        save(note);
+    }
+
+    public long getCurrentUserId() {
+        return userDetailService.getCurrentAuthenticatedUser().getId();
+    }
+
+    public User getCurrentUser() {
+        return userDetailService.getCurrentAuthenticatedUser();
+    }
+
 }
