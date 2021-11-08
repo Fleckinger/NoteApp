d8f30e48cd5d7d193b80331e4e658e065fbec73d
Vladislav Safarov <egbertz@yandex.ru>
2021-11-08 15:01:03 +0300

    delete some comments

--- src/main/java/com/fleckinger/noteapp/controller/LoginController.java
+++ src/main/java/com/fleckinger/noteapp/controller/LoginController.java
@@ -10,8 +10,6 @@ public class LoginController {
 
     @GetMapping("/login")
     public String loginPage() {
-
-        //TODO в хтмл страницы логина добавить ссылку на страницу регистрации, для тех у кого нет аккаунта
         return "login";
     }
 
