a732618a26f6126965b0096c50046f8535e6f050
Vladislav Safarov <egbertz@yandex.ru>
2021-11-11 13:51:20 +0300

    add redirect to all notes page if user already loggin in

--- src/main/java/com/fleckinger/noteapp/controller/WelcomeController.java
+++ src/main/java/com/fleckinger/noteapp/controller/WelcomeController.java
@@ -3,11 +3,16 @@ package com.fleckinger.noteapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.security.Principal;
+
 @Controller
 public class WelcomeController {
 
     @GetMapping("/")
-    public String main() {
-        return "main";
+    public String main(Principal principal) {
+        if (principal == null) {
+            return "main";
+        }
+        return "redirect:note/all";
     }
 }
