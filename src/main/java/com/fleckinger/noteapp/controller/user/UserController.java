a1ad06d4e6d14959df4cbf502b7dd3dd9b74d5c2
Vladislav Safarov <egbertz@yandex.ru>
2021-12-08 22:58:49 +0300

    Add password edition

--- src/main/java/com/fleckinger/noteapp/controller/user/UserController.java
+++ src/main/java/com/fleckinger/noteapp/controller/user/UserController.java
@@ -5,6 +5,7 @@ import com.fleckinger.noteapp.dto.user.UserDto;
 import com.fleckinger.noteapp.entity.user.User;
 import com.fleckinger.noteapp.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,10 +21,12 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/profile")
@@ -42,49 +45,27 @@ public class UserController {
 
             bindingResult.rejectValue("emailConfirm", "Emails don't match", "Emails don't match");
 
-        } else if (userDto.getEmail().equals(userService.getCurrentUser().getEmail())
+        } else if (!userDto.getEmail().equals(userService.getCurrentUser().getEmail())
                 && userService.userExists(userService.getCurrentUser().getEmail())) {
 
             bindingResult.rejectValue("emailConfirm", "That email already in use",
                     "That email already in use");
 
+        } else if (!userDto.getPassword().equals(userDto.getPasswordConfirm())) {
+            bindingResult.rejectValue("passwordConfirm", "Passwords don't match",
+                    "Passwords don't match");
         } else {
             User user = UserConverter.dtoToEntity(userDto);
             user.setId(userService.getCurrentUser().getId());
             user.setRole("ROLE_USER"); //TODO поменять роли на enum
-            user.setPassword(userService.getCurrentUser().getPassword());
+            if (userDto.getPassword().isEmpty()) {
+                user.setPassword(userService.getCurrentUser().getPassword());
+            } else {
+                user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+            }
             userService.update(user);
         }
 
-        return "userManage/profile";
-    }
-
-    @GetMapping("/profile/edit/email")
-    public String editEmail(Model model) {
-        model.addAttribute("email", "");
-        model.addAttribute("emailConfirm", "");
-        return "userManage/editEmail";
-    }
-
-    @GetMapping("/profile/edit/password")
-    public String editPassword(Model model) {
-        model.addAttribute("password", "");
-        model.addAttribute("passwordConfirm", "");
-        return "userManage/editPassword";
-    }
-
-    @PostMapping("profile/edit/password")
-    public String updatePassword(@ModelAttribute String password, @ModelAttribute String passwordConfirm,
-                                 BindingResult bindingResult) {
-        if (!password.equals(passwordConfirm)) {
-            bindingResult.rejectValue("passwordConfirm", "Passwords don't match",
-                    "Passwords don't match");
-            //TODO проверить будет ли при несовпадающих паролях зависать на той же странице или нужно добавить редирект
-        } else {
-            User user = userService.getCurrentUser();
-            user.setPassword(password);
-            userService.updatePassword(user);
-        }
         return "userManage/profile";
     }
 }
