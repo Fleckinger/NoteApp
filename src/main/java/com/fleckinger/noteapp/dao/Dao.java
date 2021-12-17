1716b47623a60ec4abb887294ba1adc3be66fdf6
Vladislav Safarov <egbertz@yandex.ru>
2021-12-17 12:57:29 +0300

    docs: add docs to core interface

--- src/main/java/com/fleckinger/noteapp/dao/Dao.java
+++ src/main/java/com/fleckinger/noteapp/dao/Dao.java
@@ -3,6 +3,10 @@ package com.fleckinger.noteapp.dao;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Interface that defines an abstract API that performs CRUD operations on objects of type T
+ * @param <T>
+ */
 public interface Dao<T> {
     Optional<T> get(Long id);
 
