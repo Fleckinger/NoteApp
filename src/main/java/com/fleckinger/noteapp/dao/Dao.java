7ade40c710caf2ee2af85ab8f127ee5975a0d3f9
Vladislav Safarov <egbertz@yandex.ru>
2021-12-17 14:06:07 +0300

    docs: Add short descriptions to Dao interface and it's methods

--- src/main/java/com/fleckinger/noteapp/dao/Dao.java
+++ src/main/java/com/fleckinger/noteapp/dao/Dao.java
@@ -4,17 +4,37 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Interface that defines an abstract API that performs CRUD operations on objects of type T
+ * Interface that defines an abstract API that performs CRUD operations
  * @param <T>
  */
 public interface Dao<T> {
+
+    /**
+     * @param id
+     * @return record from DB by uniq id
+     */
     Optional<T> get(Long id);
 
+    /**
+     * @return All records from DB as List
+     */
     List<T> getAll();
 
+    /**
+     * Saves passed as a parameter entity to the DB
+     * @param t
+     */
     void save(T t);
 
+    /**
+     * Updates entity in the DB
+     * @param t
+     */
     void update(T t);
 
+    /**
+     * Deletes entity from the DB by id
+     * @param id
+     */
     void delete(Long id);
 }
