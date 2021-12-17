cd1484d5f48e1271f93622108a6755e032d690e2
Vladislav Safarov <egbertz@yandex.ru>
2021-12-17 12:20:13 +0300

    chore: enable caching for templates

--- src/main/java/com/fleckinger/noteapp/config/SecurityWebAppInitializer.java
+++ src/main/java/com/fleckinger/noteapp/config/SecurityWebAppInitializer.java
@@ -2,5 +2,8 @@ package com.fleckinger.noteapp.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+/**
+ * Register the DelegatingFilterProxy class with the Java web server through the Spring container
+ */
 public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
 }
