27a3b13acb164432bab22c81afe6c5e49fd69005
Vladislav Safarov <egbertz@yandex.ru>
2021-12-17 14:25:55 +0300

    docs: add short description to UserConverter class

--- src/main/java/com/fleckinger/noteapp/dto/user/UserConverter.java
+++ src/main/java/com/fleckinger/noteapp/dto/user/UserConverter.java
@@ -2,16 +2,21 @@ package com.fleckinger.noteapp.dto.user;
 
 import com.fleckinger.noteapp.entity.user.User;
 
+/**
+ * Utility class to convert User entity to DTO and vice versa
+ */
 public class UserConverter {
 
     public static User dtoToEntity(UserDto dto) {
         User user = new User();
+
         user.setId(dto.getId());
         user.setFirstName(dto.getFirstName());
         user.setLastName(dto.getLastName());
         user.setEmail(dto.getEmail());
         user.setPassword(dto.getPassword());
         user.setRole(dto.getRole());
+
         return user;
     }
 
