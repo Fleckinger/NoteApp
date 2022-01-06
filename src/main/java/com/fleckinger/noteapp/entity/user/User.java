d788d3fb508caf697c4c9a72c3a93fc747e647ac
Vladislav Safarov <egbertz@yandex.ru>
2022-01-06 14:50:20 +0300

    refactor: add constructor for all fields and no args constructor

--- src/main/java/com/fleckinger/noteapp/entity/user/User.java
+++ src/main/java/com/fleckinger/noteapp/entity/user/User.java
@@ -13,6 +13,17 @@ public class User {
     private String password;
     private Role role;
 
+    public User() {
+    }
+
+    public User(long id, String firstName, String lastName, String email, String password, Role role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 
     public long getId() {
         return id;
