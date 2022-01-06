cf70f080357a9c8b24f7d1aef2b7ae1f1b0269e4
Vladislav Safarov <egbertz@yandex.ru>
2022-01-06 14:49:01 +0300

    style: remove unused imports

--- src/main/java/com/fleckinger/noteapp/config/JdbcConfig.java
+++ src/main/java/com/fleckinger/noteapp/config/JdbcConfig.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.support.JstlUtils;
 
-import javax.servlet.ServletConfig;
-import java.io.*;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+
 
 /**
  * The class represents database connect configuration.
